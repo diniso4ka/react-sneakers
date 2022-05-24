0179b8e19e858658d1a93c178d6bf4c6e76d9028
deniso4ka <den.zagrosh@gmail.com>
2022-05-25 00:03:16 +0300

    drawer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,70 @@ import s from './App.module.scss'
 function App() {
   return (
     <div className={s.wrapper}>
+      <div className={s.overlay}>
+        <div className={s.drawer}>
+          <div className={s.drawerHeader}>
+            <h2>Корзина</h2>
+            <img width={32} className={s.removeBtn} src='/img/btn-remove-focus.svg' alt='remove' />
+          </div>
+
+
+          <div className={s.items}>
+            <div className={s.cartItem}>
+              <div className={s.left}>
+                <img width={70} height={70} src='/img/sneakers/1.jpg' alt='card' />
+                <div className={s.cartInfo}>
+                  <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+                  <b>12 999 руб.</b>
+                </div>
+              </div>
+              <div>
+                <img className={s.removeBtn} src='/img/btn-remove-focus.svg' alt='remove' />
+              </div>
+            </div>
+            <div className={s.cartItem}>
+              <div className={s.left}>
+                <img width={70} height={70} src='/img/sneakers/1.jpg' alt='card' />
+                <div className={s.cartInfo}>
+                  <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+                  <b>12 999 руб.</b>
+                </div>
+              </div>
+              <div>
+                <img className={s.removeBtn} src='/img/btn-remove-focus.svg' alt='remove' />
+              </div>
+            </div>
+
+
+
+
+
+          </div>
+
+
+          <div className={s.cartTotalBlock}>
+            <ul className={s.cartList}>
+              <li className={s.price}>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li className={s.tax}>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button>Оформить заказ</button>
+          </div>
+
+
+
+
+        </div>
+      </div>
+
+
       <header>
         <div className={s.headerLeft}>
           <img src='/img/logo.png' width={40} />
@@ -27,9 +91,18 @@ function App() {
 
 
       <div className={s.content}>
-        <h1>Все кроссовки</h1>
+        <div className={s.contentHeader}>
+          <div>
+            <h1>Все кроссовки</h1>
+          </div>
+          <div className={s.searchBlock}>
+            <img src='/img/search.svg' alt='' />
+            <input placeholder='Поиск...' />
+          </div>
+        </div>
         <div className={s.sneakers}>
           <div className={s.card}>
+
             <div className={s.cardWrapper}>
               <img className={s.cardImage} src='/img/sneakers/1.jpg' width={133} height={112} />
               <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
@@ -39,10 +112,12 @@ function App() {
                   <b>12 999 руб.</b>
                 </div>
                 <button className={s.button}>
-                  <img src='/img/plus.svg' width={11} />
+                  <img src='/img/plus.svg' alt='' width={11} />
                 </button>
               </div>
             </div>
+            <img className={s.heart} src='/img/heart-unliked.svg' alt='unliked' />
+
           </div>
 
           <div className={s.card}>
@@ -132,7 +207,7 @@ function App() {
 
 
       </div>
-    </div>
+    </div >
   );
 }
 
